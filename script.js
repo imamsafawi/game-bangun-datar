7a0ed55edc648500cbdc5f18c65fe1c1d0ee6b13
imamsafawi <159208244+imamsafawi@users.noreply.github.com>
2024-09-15 21:36:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+﻿body {
+    font-family: Arial, sans-serif;
+    background-color: #f4f4f4;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+}
+
+.container {
+    background-color: white;
+    padding: 20px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    width: 300px;
+    text-align: center;
+}
+
+#question-area {
+    margin-bottom: 10px;
+}
+
+#timer {
+    font-size: 18px;
+    font-weight: bold;
+    color: red;
+    margin-top: 10px;
+}
+
+#result {
+    color: green;
+    font-size: 16px;
+    margin-top: 10px;
+}
+
+#score {
+    font-size: 18px;
+    margin-top: 10px;
+}
+
+#trophy-area {
+    display: none;
+}
+
+.hidden {
+    display: none;
+}
+
+#trophy {
+    width: 100px;
+    margin-top: 20px;
+}
